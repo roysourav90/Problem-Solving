00893714c9afb524a97f5a891730f48defe285f2
roysourav90 <76598689+roysourav90@users.noreply.github.com>
2024-05-06 02:50:38 -0400

    Update Contains Duplicate.py

--- Contains Duplicate.py
+++ Contains Duplicate.py
@@ -1,8 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        hashset = set()      
-        for num in nums:
+        hashset = set()                # Set named 'hashed' initialized to store unique elements                                        
+        for num in nums:               # Method iterates through each element num in the list nums. If num is already present in the hashset,                                         
             if num in hashset:
-                return True
-            hashset.add(num)
-        return False
+                return True            #it means it's a duplicate, so the method immediately returns True.
+            hashset.add(num)           #If num is not already in hashset, it adds num to the set using the add method.
+        return False                   # If the loop completes without finding any duplicates then return false
